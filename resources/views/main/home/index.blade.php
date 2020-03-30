c6b0eec6cc68f47eaec55b2e77638e3e804223ae
ft17 <valery@churbanov74.ru>
2020-03-30 16:13:56 +0400

    Beatify blade files

--- resources/views/main/home/index.blade.php
+++ resources/views/main/home/index.blade.php
@@ -1,28 +1,25 @@
 <!-- навигационная панель в header'e -->
 <div class="uk-section-primary uk-panel uk-light uk-padding-small uk-margin-medium-bottom">
-    <!-- основной контейнер навигационной панели -->
-    <div class="uk-container uk-container-small uk-align-center">
-         <!-- логотип с описанием -->
-         <div class="logo">
-             <h4 class="uk-text-center uk-padding-remove uk-margin-remove">
-                 <a href="/" class="uk-link-heading">
-                    <img src="/assets/images/logo.svg" alt="" class="logo__img">
-                     @lang('main_page_content.nameProject')
-                 </a>
-             </h4>
-         </div>
-        <!-- конец - логотип с описанием -->
+  <!-- основной контейнер навигационной панели -->
+  <div class="uk-container uk-container-small uk-align-center">
+    <!-- логотип с описанием -->
+    <div class="logo">
+      <h4 class="uk-text-center uk-padding-remove uk-margin-remove">
+        <a href="/" class="uk-link-heading">
+          <img src="/assets/images/logo.svg" alt="" class="logo__img">
+          @lang('main_page_content.nameProject')
+        </a>
+      </h4>
     </div>
-    <!-- конец -основной контейнер навигационной панели -->
+    <!-- конец - логотип с описанием -->
+  </div>
+  <!-- конец -основной контейнер навигационной панели -->
 </div>
 <!-- конец - навигационная панель в header'е -->
-<login-and-registration-component
-    type-form={{ session('typeForm') }}
-    :props-array="{{ json_encode(__('main_page_content.startPage'), JSON_UNESCAPED_UNICODE) }}"
-    :form-route-login="{{ json_encode(route('authenticate'), JSON_UNESCAPED_UNICODE) }}"
-    :form-route-registration="{{ json_encode(route('registration'), JSON_UNESCAPED_UNICODE) }}"
-    old-login="{{ old('login') }}"
-    old-reg-login="{{ old('reg-login') }}"
-    old-name="{{ old('name') }}"
-    old-errors="{{ $errors }}">
+<login-and-registration-component type-form={{ session('typeForm') }}
+  :props-array="{{ json_encode(__('main_page_content.startPage'), JSON_UNESCAPED_UNICODE) }}"
+  :form-route-login="{{ json_encode(route('authenticate'), JSON_UNESCAPED_UNICODE) }}"
+  :form-route-registration="{{ json_encode(route('registration'), JSON_UNESCAPED_UNICODE) }}"
+  old-login="{{ old('login') }}" old-reg-login="{{ old('reg-login') }}" old-name="{{ old('name') }}"
+  old-errors="{{ $errors }}">
 </login-and-registration-component>
--- resources/views/main/layout/header-app.blade.php
+++ resources/views/main/layout/header-app.blade.php
@@ -2,35 +2,25 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Покер</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    {{-- <title>{{ $title }}</title> --}}
-
-    <title>Покер</title>
-
-
-    <!-- Scripts -->
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
-    {{-- <script src="{{ mix('js/app.js') }}"></script> --}}
-    <link href="https://fonts.googleapis.com/css?family=Fira+Mono&amp;subset=cyrillic" rel="stylesheet">
-    {{-- <script src="http://localhost:8080/js/app.js"></script> --}}
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+  <link href="https://fonts.googleapis.com/css?family=Fira+Mono&amp;subset=cyrillic" rel="stylesheet">
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
-    <div id="app">
-        @yield('content')
-    </div>
+  <div id="app">
+    @yield('content')
+  </div>
 </body>
 
 </html>
